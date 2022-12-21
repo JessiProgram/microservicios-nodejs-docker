0a4f7465635b94beb41e1197c3497fb517e1e417
JessiProgram <jessicala182@gmail.com>
2022-12-21 12:01:26 -0300

    nodemon y servidor automatico sensible a cambios

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware, fixRequestBody } = require('http-proxy-middleware');
 
 const app = express();
 const port = 3007;
 
-app.use('/orders', createProxyMiddleware({ target: 'http://host.docker.internal:3005', changeOrigin: true }));
-app.use('/products', createProxyMiddleware({ target: 'http://host.docker.internal:3006', changeOrigin: true }));
+app.use('/orders', createProxyMiddleware({ target: 'http://host.docker.internal:3005', 
+changeOrigin: true, onProxyReq: fixRequestBody}));
+app.use('/products', createProxyMiddleware({ target: 'http://host.docker.internal:3006', 
+changeOrigin: true, onProxyReq: fixRequestBody }));
 app.get('/', (req, res) => res.send('Hola Gateway API'));
 
 app.listen(port, () => console.log(`App de ejemplo en el puerto ${port}!`));
